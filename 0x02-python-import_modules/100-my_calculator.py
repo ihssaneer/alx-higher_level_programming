ac5b825b67c90253f4a4c4c70954c1087dbd8dd3
ihssaneer <errajy.ihssane@gmail.com>
2024-01-04 22:18:41 +0100

    Creating files

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+from sys import argv
+from calculator_1 import add, sub, mul, div
+if __name__ == "__main__":
+    if len(argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+    else:
+        a = int(argv[2])
+        b = int(argv[4])
+        if argv[3] == '+':
+            print("{}".format(add(a,b)))
+        elif argv[3] == '-':
+            print("{}".format(sub(a,b)))
+        elif argv[3] == '*':
+            print("{}".format(mul(a,b)))
+        elif argv[3] == '/':
+            print("{}".format(add(a,b)))
+        else:
+            
+    
