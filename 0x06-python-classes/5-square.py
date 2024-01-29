afb5ff9ee32781fe5fc1c3de498b4819b035568f
ihssaneer <errajy.ihssane@gmail.com>
2024-01-29 23:17:33 +0100

    Creating F 5

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+"""This module contain the class Square."""
+
+
+class Square:
+    """
+    This class Square that defines a square
+    by private instance attribute: size, raise
+    error if size is not int or < 0 and contain
+    a getter, a setter and an area methode.
+    """
+
+    def __init__(self, size=0):
+        """Initialize a new Square.
+        Args:
+            size (int): The size of the new square.
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """Return the size.
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Sets the size.
+        Args:
+            value (int) : the value to attribute to size.
+        """
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """Calculate the area of a square.
+        """
+        return self.__size * self.__size
+
+    def my_print(self):
+        """prints in stdout the square with the character #.
+        """
+        if self.__size == 0:
+            print()
+        else:
+            for x in range(self.__size):
+                print("#" * self.__size)
