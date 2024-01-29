b2dda518300dcf947b6707ec845a3250d528ee07
ihssaneer <errajy.ihssane@gmail.com>
2024-01-29 22:59:58 +0100

    Creating F 4

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -7,19 +7,34 @@ class Square:
     This class Square that defines a square
     by private instance attribute: size, raise
     error if size is not int or < 0 and contain
-    area methode.
+    a getter, a setter and an area methode.
     """
+
     def __init__(self, size=0):
         """Initialize a new Square.
         Args:
             size (int): The size of the new square.
         """
-        if type(size) is not int:
+        self.size = size
+
+    @property
+    def size(self):
+        """Return the size.
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Sets the size.
+        Args:
+            value (int) : the value to attribute to size.
+        """
+        if type(value) is not int:
             raise TypeError("size must be an integer")
-        elif size < 0:
+        elif value < 0:
             raise ValueError("size must be >= 0")
         else:
-            self.__size = size
+            self.__size = value
 
     def area(self):
         """Calculate the area of a square.
