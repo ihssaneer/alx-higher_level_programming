7326516b59e080877d5b6132d04fd59fecb7802b
ihssaneer <errajy.ihssane@gmail.com>
2024-01-29 18:36:47 +0100

    Creating F 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" This module contains a class Square."""
+
+
+class Square:
+    """
+    This class Square that defines a square
+    by private instance attribute: size.
+    """
+    def __init__(self, size):
+        """Initialize a new Square.
+        Args:
+            size (int): The size of the new square.
+        """
+        self.__size = size
