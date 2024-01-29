b29c8c4ef3ddc8f0eb49a223588a6b10d9bd96b5
ihssaneer <errajy.ihssane@gmail.com>
2024-01-29 22:38:19 +0100

    Creating F 3

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,12 +1,13 @@
-#!/usr/bin/python3
-""" This module contains a class Square."""
+#!/bin/usr/python3
+"""This module contain the class Square."""
 
 
 class Square:
     """
     This class Square that defines a square
-    by private instance attribute: size and raise
-    error if size is not int or < 0.
+    by private instance attribute: size, raise
+    error if size is not int or < 0 and contain
+    area methode.
     """
     def __init__(self, size=0):
         """Initialize a new Square.
@@ -19,3 +20,8 @@ class Square:
             raise ValueError("size must be >= 0")
         else:
             self.__size = size
+
+    def area(self):
+        """Calculate the area of a square.
+        """
+        return self.__size * self.__size
