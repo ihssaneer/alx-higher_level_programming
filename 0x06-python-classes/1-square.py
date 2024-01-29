c6c340ca08f59c089f26041f4b68f830b169164f
ihssaneer <errajy.ihssane@gmail.com>
2024-01-29 18:59:38 +0100

    Modifying F 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -7,7 +7,7 @@ class Square:
     This class Square that defines a square
     by private instance attribute: size.
     """
-    def __init__(self, size):
+    def __init__(self, size=0):
         """Initialize a new Square.
         Args:
             size (int): The size of the new square.
